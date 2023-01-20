0fec1b385b2ceaa52207aa8a70c0fcdd96b21357
mmeiring021 <mmeiring021@student.wethinkcode.co.za>
2023-01-20 15:54:35 +0200

    Data class to get events from the database

---
+++ common/src/main/kotlin/za/co/themainevents/models/Event.kt
@@ -0,0 +1,9 @@
+package za.co.themainevents.models
+
+data class Event (
+    val fromPersonEmail: String,
+    val location: String,
+    val date: String,
+    val time: String,
+    val description: String
+)
