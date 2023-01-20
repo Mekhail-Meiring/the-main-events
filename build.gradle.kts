d84dc1525e4c205245f9d7f966bfdb32b7c63c4b
mmeiring021 <mmeiring021@student.wethinkcode.co.za>
2023-01-20 15:48:42 +0200

    removed comments and white spaces, also added retrofit and retrofit gson convertor dependency in the main gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,6 +45,10 @@ allprojects{
 		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 		implementation("org.jetbrains.kotlin:kotlin-reflect")
 		implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+		implementation("com.squareup.retrofit2:retrofit:2.9.0")
+		implementation("com.squareup.retrofit2:converter-gson:2.1.0")
 		testImplementation("org.springframework.boot:spring-boot-starter-test")
+
 	}
+
 }
