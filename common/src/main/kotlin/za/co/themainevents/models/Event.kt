1aeb5425c8bc547e27ede911c0df0716df75fa7a
mmeiring021 <mmeiring021@student.wethinkcode.co.za>
2023-01-26 18:12:39 +0200

    Added more fields to the data class and as well as the JsonProperty annotation for json serialization

--- common/src/main/kotlin/za/co/themainevents/models/Event.kt
+++ common/src/main/kotlin/za/co/themainevents/models/Event.kt
@@ -1,9 +1,24 @@
 package za.co.themainevents.models
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class Event (
-    val fromPersonEmail: String,
+
+    @JsonProperty("event_id")
+    val eventId: Int,
+
+    @JsonProperty("from_client_id")
+    val fromClientId: Int,
+
+    @JsonProperty("location")
     val location: String,
+
+    @JsonProperty("date")
     val date: String,
+
+    @JsonProperty("time")
     val time: String,
+
+    @JsonProperty("description")
     val description: String
 )
