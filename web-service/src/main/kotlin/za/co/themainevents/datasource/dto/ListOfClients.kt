02f0ec475c04cc768822299334d6b52f6c38c8ac
mmeiring021 <mmeiring021@student.wethinkcode.co.za>
2023-01-26 18:15:57 +0200

    Data transfer objects to get data from the rest api's

---
+++ web-service/src/main/kotlin/za/co/themainevents/datasource/dto/ListOfClients.kt
@@ -0,0 +1,7 @@
+package za.co.themainevents.datasource.dto
+
+import za.co.themainevents.models.Client
+
+data class ListOfClients (
+    val listOfClients: List<Client>
+    )
---
+++ web-service/src/main/kotlin/za/co/themainevents/datasource/dto/ListOfEvents.kt
@@ -0,0 +1,7 @@
+package za.co.themainevents.datasource.dto
+
+import za.co.themainevents.models.Event
+
+data class ListOfEvents (
+    val listOfEvents: List<Event>
+    )
