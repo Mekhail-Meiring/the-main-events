31ea5013fea1d404d5846909859e58a633965f0b
mmeiring021 <mmeiring021@student.wethinkcode.co.za>
2023-01-29 01:36:34 +0200

    Added dependencies Specific to the module Client service. For ORM and testing the database

--- client-service/build.gradle.kts
+++ client-service/build.gradle.kts
@@ -1,4 +1,36 @@
 
+extra["testcontainersVersion"] = "1.17.6"
+
 dependencies{
+
+    val postgresVersion = "42.3.1"
+    val jetbrainsExposedVersion = "0.40.1"
+
+    // Common module for project libraries:
     implementation(project(":common"))
+
+    // Hikari:
+    implementation("com.zaxxer:HikariCP:3.4.5")
+
+    // Postgresql dbc driver:
+    runtimeOnly("org.postgresql:postgresql:$postgresVersion")
+
+    // Jetbrains Exposed:
+    implementation("org.jetbrains.exposed:exposed-core:$jetbrainsExposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$jetbrainsExposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$jetbrainsExposedVersion")
+
+    // Spring:
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+
+    // TestContainers:
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
+
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.testcontainers:testcontainers-bom:${property("testcontainersVersion")}")
+    }
 }
